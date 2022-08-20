a3579f37a3f58acdbd4bfc5e050fb32c54e51e6a
AravindSamala <aravind.samala96@gmail.com>
2022-08-20 14:30:23 +0530

    Add Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className='text-3xl font-bold text-red-700'>Hello World</h1>
+  )
+}
+
+export default Home
