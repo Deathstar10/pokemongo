6e160348116735237d1add77f9469c38e200cb2e
AravindSamala <aravind.samala96@gmail.com>
2022-08-22 23:05:01 +0530

    Add src folder and change tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <h1 className='text-3xl font-bold text-red-700'>Hello World</h1>
+    <h1 className='text-3xl font-bold'>Which Pokemon is Roundest?</h1>
+    
   )
 }
 
